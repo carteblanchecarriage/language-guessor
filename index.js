2723350da81be026537320a88286f3a9354ba833
carteblanchecarriage <carteblanchecarriage@protonmail.com>
2023-01-03 18:23:57 -0500

    Adding files

---
+++ index.js
@@ -0,0 +1,21 @@
+// Make "require" available
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+// import and require modules
+import { franc } from 'franc'
+const langs = require("langs")
+const colors = require("colors");
+
+const input = process.argv[2];
+const langCode = langs.where("3", franc(input));
+
+if (langCode === undefined) {
+    console.log('error'.red)
+} else {
+    const language = langCode.name;
+    const response = `${language} is most likely the language`;
+    console.log(response.green);
+    console.log(response.rainbow);
+    console.log(response.rainbow);
+}
